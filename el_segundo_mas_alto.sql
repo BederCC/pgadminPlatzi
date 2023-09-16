28ec3fa2ef0d4cf6575e287c409e0d53bdbac540
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-09-16 04:47:35 +0000

    reto2

--- el_segundo_mas_alto.sql
+++ el_segundo_mas_alto.sql
@@ -46,3 +46,10 @@ FROM platzi.alumnos
 OFFSET (
 	SELECT COUNT(id)/2 FROM platzi.alumnos
 );
+
+SELECT ROW_NUMBER() OVER() AS row_id, *
+FROM platzi.alumnos
+OFFSET(
+	SELECT COUNT(*)/2
+	FROM platzi.alumnos
+);
