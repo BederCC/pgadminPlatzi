e6690c1d7890b643f08f24d927e587ea5aab05e2
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-09-18 23:53:59 -0500

    Update en_mis_tiempos.sql

--- en_mis_tiempos.sql
+++ en_mis_tiempos.sql
@@ -8,3 +8,10 @@ SELECT DATE_PART('YEAR', fecha_incorporacion) as anio_ncorporacion,
 	DATE_PART('MONTH', fecha_incorporacion) AS mes_incorporacion,
 	DATE_PART('DAY', fecha_incorporacion) AS dia_incorporacion
 FROM platzi.alumnos;
+
+--RETO
+SELECT DATE_PART('HOUR', fecha_incorporacion) AS hora_incorporacion,
+		DATE_PART('MINUTE', fecha_incorporacion) AS minuto_incorporacion,
+		DATE_PART('SECOND', fecha_incorporacion) AS segundo_incorporacion
+FROM platzi.alumnos;
+		
