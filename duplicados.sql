d1841c2e08a426484e2961ad39fb6fe234d27de8
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-10-04 23:32:35 -0500

    Update duplicados.sql

--- duplicados.sql
+++ duplicados.sql
@@ -51,3 +51,26 @@ FROM(
 	FROM platzi.alumnos
 ) AS duplicados
 WHERE DUPLICADOS.row > 1;
+
+--Eliminar los elementos duplicados
+DELETE FROM platzi.alumnos
+WHERE id IN (
+SELECT *
+FROM(
+	SELECT id,
+	ROW_NUMBER() OVER(
+		PARTITION BY
+			nombre,
+			apellido,
+			email,
+			colegiatura,
+			fecha_incorporacion,
+			carrera_id,
+			tutor_id
+		ORDER BY id ASC
+	) AS row,
+	*
+	FROM platzi.alumnos
+) AS duplicados
+WHERE duplicados.row > 1
+);
