8602e57ce09676f3dcdd85883fbefad7fe010f15
Beder Danilo Casa Condori <108836354+BederCC@users.noreply.github.com>
2023-10-12 23:44:14 -0500

    Update selector_rango.sql

--- selector_rango.sql
+++ selector_rango.sql
@@ -20,3 +20,17 @@ SELECT ISEMPTY(numrange(1,5));
 SELECT * 
 FROM platzi.alumnos
 WHERE int4range(10,20) @> tutor_id;
+
+
+/**
+  * RETO:   Encontrar los números en común entre
+  *         los id de tutor y carrera;
+  */
+
+SELECT numrange(
+	(SELECT min(tutor_id) FROM platzi.alumnos),
+	(SELECT max(tutor_id) FROM platzi.alumnos)
+) * numrange(
+	(SELECT min(carrera_id) FROM platzi.alumnos),
+	(SELECT max(carrera_id) FROM platzi.alumnos)
+);
